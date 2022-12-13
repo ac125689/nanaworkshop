feac128c29c16503c2c105d0359124577ea16460
ac125689 <81823797+ac125689@users.noreply.github.com>
2022-12-12 21:57:14 -0500

    Update nana_workshop.py

---
+++ nana_workshop.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from pandas import DataFrame
+from google.oauth2 import service_account
+from gspread_pandas import Spread,Client
+from streamlit_option_menu import option_menu
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+# Create a connection object.
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Create a Google Authentication connection object
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"], scopes = scope)
+client = Client(scope=scope,creds=credentials)
+spreadsheetname = "pujalu"
+spread = Spread(spreadsheetname,client = client)
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+sh = client.open(spreadsheetname)
+worksheet_list = sh.worksheets()
+
+
+def worksheet_names():
+    sheet_names = []   
+    for sheet in worksheet_list:
+        sheet_names.append(sheet.title)  
+    return sheet_names
+
+def load_the_spreadsheet(spreadsheetname):
+    worksheet = sh.worksheet(spreadsheetname)
+    df = DataFrame(worksheet.get_all_records())
+    return df
+def update_the_spreadsheet(spreadsheetname,dataframe):
+    col = ['Date','Name', 'Address', 'Number', 'Name of puja','Names Items I need to get']
+    spread.df_to_sheet(dataframe[col],sheet = spreadsheetname,index = False)
+
+def main():
+    st.title("Home page")
+
+if __name__ == "__main__":
+  main()
