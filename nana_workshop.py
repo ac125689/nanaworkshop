03cea52ade8add1f9e5609914796919355a8a8ef
ac125689 <81823797+ac125689@users.noreply.github.com>
2022-12-14 15:17:27 -0500

    Update nana_workshop.py

--- nana_workshop.py
+++ nana_workshop.py
@@ -85,7 +85,9 @@ def main():
             st.image(puja_icon('images.jpeg'))
             with st.expander('Regular Homam list'):
                 st.write("Name of the item | Quantity")
-                st.write("Turmeric powder | one small packet")
+                st.write("Turmeric powder  | one small packet")
+                st.write("Kumkum           | one small packet")
+                
     # Chat with Priest code 
     if selected == 'Chat with Priest':
         st.title('QR code')
