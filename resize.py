30e3cde5d996d232a0933253293aca400042274a
ac125689 <81823797+ac125689@users.noreply.github.com>
2022-12-15 14:49:24 -0500

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,2 +1,9 @@
 from PIL import Image
 
+image1 = Image.open('image/puja_icons/engagement-rings.jpeg')
+
+print(image1.size)
+
+new_hight  = 219
+new_width = int(new_hight / image1.height * image1.width)
+print(new_width)
