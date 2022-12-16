7b24b9f12c80aa86af5a7c29ca353fd37f86b534
ac125689 <81823797+ac125689@users.noreply.github.com>
2022-12-15 19:22:43 -0500

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,13 +1,9 @@
 from PIL import Image
 
-image1 = Image.open('image/puja_icons/engagement-rings.jpeg')
+image1 = Image.open('image/puja_icons/Chandi.jpeg')
 
 print(image1.size)
 
 new_hight  = 219
 new_width = int(new_hight / image1.height * image1.width)
 print(new_width)
-
-re_im = image1.resize(219,220)
-
-re_im.show()
