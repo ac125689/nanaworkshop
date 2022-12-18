2f3d65cd32a03c08794d2acfdc64c45f3fdaff7d
ac125689 <81823797+ac125689@users.noreply.github.com>
2022-12-18 03:04:18 -0500

    h

--- nana_workshop.py
+++ nana_workshop.py
@@ -1,26 +1,35 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 from puja_list import puja_list
-from dont_mess import home,config,Chef,Priest,hide_st,select
+from dont_mess import home,config,Chef,Priest,hide_st
 from puja_sign_up import puja_sign
 
 config()
 hide_st()
 def main():
-    select()
+    st.cache()
+    with st.sidebar:
+        selected = option_menu(
+            menu_title=None,
+            options=['Home', 'Puja Samagri list', 'Puja sign-up form','Chat with Priest', 'Chat with the Chef'],
+            icons=['house-door-fill','list', 'card-checklist','chat-dots-fill','chat-dots'],
+            menu_icon='cast',
+            default_index=0
+    )
     # Home code
-    if select() == 'Home':
+    if selected == 'Home':
         home()
     # Puja Samagri list code
-    if select() == 'Puja Samagri list':
+    if selected == 'Puja Samagri list':
         puja_list()
     # Puja sign-up form
-    if select() == 'Puja sign-up form':
+    if selected == 'Puja sign-up form':
         puja_sign()
     # Chat with Priest code 
-    if select() == 'Chat with Priest':
+    if selected == 'Chat with Priest':
         Priest()
     # Chat with the Chef code
-    if select() == 'Chat with the Chef':
+    if selected == 'Chat with the Chef':
         Chef()
     # Don't mess with the Chat with Priest and Chat with the Chef code.
 if __name__ == "__main__":
