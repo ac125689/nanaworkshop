b4ec823988a00f93c147fe6327ba04c11e3f0c1c
ac125689 <81823797+ac125689@users.noreply.github.com>
2022-12-17 20:37:16 -0500

    Update nana_workshop.py

--- nana_workshop.py
+++ nana_workshop.py
@@ -703,8 +703,8 @@ def main():
             st.info("The cost priest service will be ")
             st.download_button(
                     label="Download Homam list",
-                    data=puja_list_down('Homam list.csv'),
-                    file_name= "Homam list.csv",
+                    data=puja_list_down('Regular Homam list.csv'),
+                    file_name= "Regular Homam list.csv",
                     mime='text/csv'
                 )
         if name_of_puja == 'Chandi Homam':
