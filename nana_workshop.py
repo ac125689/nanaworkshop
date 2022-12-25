4296242c43ef7bd37be3bdc633629961fcbd3898
ac125689 <atirumala844@gmail.com>
2022-12-24 21:30:55 -0500

    s

--- nana_workshop.py
+++ nana_workshop.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import streamlit.components.v1 as stc
-import codecs
 from streamlit_option_menu import option_menu
 from puja_list import puja_list
 from dont_mess import home,config,Chef,Priest,hide_st
@@ -8,24 +6,6 @@ from puja_sign_up import puja_sign
 
 config()
 hide_st()
-def B_T_P_botton():
-    file = codecs.open('btn.html','r')
-    page = file.read()
-    stc.html(page,scrolling=False)
-def btn():
-    bt ="""
-    <script>
-            const btnTTf = document.querySelector("#btnTT");
-            btnTTf.addEventListener("click", function(){
-            //window.scrollTo(0,0);
-            window.scrollTo({
-                top:0,
-                left:0,
-                behavior:"smooth"
-            });
-            });
-    </script>
-    """
 def main():
     st.cache()
     with st.sidebar:
@@ -42,12 +22,9 @@ def main():
     # Puja Samagri list code
     if selected == 'Puja Samagri list':
         puja_list()
-        B_T_P_botton()
-        st.button('Top',on_click='https://sri-prasanna-venkateswara-priest-and-food-services.streamlit.app/#home')
     # Puja sign-up form
     if selected == 'Puja sign-up form':
         puja_sign()
-        B_T_P_botton()
     # Chat with Priest code 
     if selected == 'Chat with Priest':
         Priest()
